77f8a2ba47d1ba019a3102ea04c21d76fdb14103
galagaking <frank@beksonline.nl>
2016-10-15 00:06:24 +0200

    Remove update from github

--- install.sh
+++ install.sh
@@ -15,17 +15,17 @@ echo "The Things Network Gateway installer"
 echo "Version $VERSION"
 
 # Update the gateway installer to the correct branch (defaults to master)
-echo "Updating installer files..."
-OLD_HEAD=$(git rev-parse HEAD)
-git fetch
-git checkout -q $VERSION
-git pull
-NEW_HEAD=$(git rev-parse HEAD)
+# echo "Updating installer files..."
+# OLD_HEAD=$(git rev-parse HEAD)
+# git fetch
+# git checkout -q $VERSION
+# git pull
+# NEW_HEAD=$(git rev-parse HEAD)
 
-if [[ $OLD_HEAD != $NEW_HEAD ]]; then
-    echo "New installer found. Restarting process..."
-    exec "./install.sh" "$VERSION"
-fi
+# if [[ $OLD_HEAD != $NEW_HEAD ]]; then
+#    echo "New installer found. Restarting process..."
+#    exec "./install.sh" "$VERSION"
+# fi
 
 # Request gateway configuration data
 # There are two ways to do it, manually specify everything
